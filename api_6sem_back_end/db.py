133056f485350ed43ec703ef9ce52c192fb8c28c
Amandavannuccic <vannucci_job@hotmail.com>
2025-09-10 23:36:46 -0300

    LMN-39 feat: remove .env

--- api_6sem_back_end/db.py
+++ api_6sem_back_end/db.py
@@ -2,4 +2,4 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 client = AsyncIOMotorClient("mongodb://localhost:27017")
 db = client["testluminia"]
-collection = db["chamados"]
+collection = db["testluminia"]
